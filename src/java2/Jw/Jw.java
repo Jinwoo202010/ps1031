89076364f54a78182de73bff658c28570314733a
Jinwoo202010 <Krewyat7419@gmail.com>
2020-10-31 16:32:10 +0900

    ignore , test

---
+++ src/java2/Jw/Jw.java
@@ -0,0 +1,19 @@
+package java2.Jw;
+
+import java.util.Scanner;
+
+public class Jw {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("aa");
+		
+		// 1. 입력값 받기 
+		String str = sc.nextLine();
+        System.out.println(str);
+		
+	}
+
+}
